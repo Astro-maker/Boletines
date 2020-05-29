4a9ae11048656efc7d29db4129d38fa542a4a0b9
Astro-maker <juanitoalcachof57@gmail.com>
2020-05-29 10:13:37 -0500

    Cambiando al proyecto de boletines

---
+++ assets/scripts/main.js
@@ -0,0 +1,22 @@
+let links= document.querySelectorAll("a")
+
+
+links.forEach(function(link){
+  link.addEventListener('click',function(ev){
+    ev.preventDefault()
+
+    let content = document.querySelector(".content")
+
+    content.classList.remove("animate__animated")
+    content.classList.remove("animate__fadeInDown")
+
+    content.classList.add("animate__animated")
+    content.classList.add("animate__fadeOutUp")
+
+    setTimeout(function(){
+      location.href = "../index.html"
+    },1000)
+
+    return false
+  })
+})
